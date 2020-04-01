6a7975261d989569293286d1499aa4bef03c3f51
Umashri R <uma-6513@uma-6513.localzoho.com>
2020-04-01 20:32:27 +0530

    Pod headers added

--- Example/Pods/Headers/Public/ZCRMiOS/Bridge-Header.h
+++ Example/Pods/Headers/Public/ZCRMiOS/Bridge-Header.h
@@ -1,1 +1,13 @@
-../../../../../ZCRMiOS/Classes/Bridge-Header.h
+//
+//  Bridge-Header.h
+//  Pods
+//
+//  Created by Boopathy P on 21/02/18.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+
+#include <sqlite3.h>
+
+#endif /* Bridge_Header_h */
